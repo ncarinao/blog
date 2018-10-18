bc7b995c0c624a678b91851e3a32580cfd08a1da
ncarinao <ncarinao@gmail.com>
2018-10-17 22:15:40 -0300

    blog

---
+++ README.md
@@ -0,0 +1,4 @@
+blog
+====
+
+A Symfony project created on October 1, 2018, 9:14 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Post;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $posts = $em->getRepository('AppBundle:Post')->findAll();
+
+        return $this->render('post/index.html.twig', array(
+            'posts' => $posts,
+        ));
+    }
+}
